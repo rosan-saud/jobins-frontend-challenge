83b9508779d49cdf93986abe9492229ceb251c6d
Roshan Saud <rosansaud5@gmail.com>
2023-06-03 13:24:30 +0545

    feat: setup for jobins-frontend-challenge

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<>
+			<h1 className="text-3xl underline font-bold text-center text-blue-900">
+				Jobins Frontend Challenge
+			</h1>
+		</>
+	);
+}
+
+export default App;
